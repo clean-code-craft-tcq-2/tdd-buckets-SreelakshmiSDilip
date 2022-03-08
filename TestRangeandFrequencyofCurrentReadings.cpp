1240ed719a8adb60e9f3696b9ed4e8423aeb6e41
SreelakshmiSDilip <91017915+SreelakshmiSDilip@users.noreply.github.com>
2022-03-08 21:28:15 +0530

    Update TestRangeandFrequencyofCurrentReadings.cpp

--- TestRangeandFrequencyofCurrentReadings.cpp
+++ TestRangeandFrequencyofCurrentReadings.cpp
@@ -1,15 +1,15 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 
-#include "catch.hpp"
+#include "test/catch.hpp"
 #include "RangeandFrequencyofCurrentReadings.h"
 
 TEST_CASE("get the range and count from the readings ") {
  int testInput[9] = {2,3,4,5,8,9,9,10,12};
  vector<StringandNumRangeOutput> Observed_range_count_list;
  vector<StringandNumRangeOutput> Expected_range_count_list;
- Expected_range_count_list[0]->numIndexAndCount = {2,5,4};
- Expected_range_count_list[1]->numIndexAndCount ={8,10,4};
- Expected_range_count_list[2]->numIndexAndCount ={12,1};
+ Expected_range_count_list[0].numIndexAndCount = {2,5,4};
+ Expected_range_count_list[1].numIndexAndCount ={8,10,4};
+ Expected_range_count_list[2].numIndexAndCount ={12,c_NOTVALID,1};
 
  Observed_range_count_list = GetRangesAndFrequentValue(testInput,9);
 
@@ -25,7 +25,7 @@ TEST_CASE("Get the range and count values combined as a string ") {
 	int testReadings[9] = {2,3,4,5,8,9,9,10,12};
 	RangeIndicesandCount testInput1 = {2,5,4};
 	RangeIndicesandCount testInput2 = {8,10,4};
-	RangeIndicesandCount testInput3 = {12};
+	RangeIndicesandCount testInput3 = {12,c_NOTVALID};
  
   REQUIRE(GetCombinedRangeandCountString(testInput1,testReadings) == "[2-5]->4");
   REQUIRE(GetCombinedRangeandCountString(testInput2,testReadings)  == "[8-10]->4");
