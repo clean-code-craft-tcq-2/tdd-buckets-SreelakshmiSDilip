84f0f4f53e75f34a3e241652b55c9790fabdb8ee
SreelakshmiSDilip <91017915+SreelakshmiSDilip@users.noreply.github.com>
2022-03-09 00:11:55 +0530

    Update RangeandFrequencyofCurrentReadings.cpp

--- RangeandFrequencyofCurrentReadings.cpp
+++ RangeandFrequencyofCurrentReadings.cpp
@@ -18,65 +18,66 @@ vector<StringandNumRangeOutput> GetRangesAndFrequentValue(int inputValues[],int 
 	//find the ranges
 	vector<StringandNumRangeOutput> combined_range_count_list;
 
-     
-    // If the array is empty,
-    // return the range_list
-    if (numOfElements == 0)
-    {
-        return combined_range_count_list;
-    }
-     
-    // Traverse the array from first position
-    for(int iter = 1; iter <= numOfElements; iter++)
-    {
-		
+
+	// If the array is empty,
+	// return the range_list
+	if (numOfElements == 0)
+	{
+		return combined_range_count_list;
+	}
+
+	// Traverse the array from first position
+	for(int iter = 1; iter <= numOfElements; iter++)
+	{
+
 		StringandNumRangeOutput rangeFromReadingOutput;
-     
-        // Check the difference between the
-        // current and the previous elements
-        // If the difference doesn't equal to 1
-        // just increment the length variable.
-        if (iter == numOfElements || inputValues[iter] - inputValues[iter - 1] != 1)
-        {
-     
-            // If the range contains
-            // only one element.
-            // add it into the range_list.
+
+		// Check the difference between the
+		// current and the previous elements
+		// If the difference doesn't equal to 1
+		// just increment the length variable.
+		if (iter == numOfElements || inputValues[iter] - inputValues[iter - 1] != 1)
+		{
+
+			// If the range contains
+			// only one element.
+			// add it into the range_list.
 			RangeValuesAndCount currentRangeandCount;
 			currentRangeandCount.rangeLower=  inputValues[iter - length];
-            currentRangeandCount.rangeUpper =  inputValues[iter - 1];
+			currentRangeandCount.rangeUpper =  inputValues[iter - 1];
 			currentRangeandCount.count = length;
+             
 
-			
-            string rangeString = GetRangeandCountOutputAsString(currentRangeandCount,inputValues);
+			string rangeString = GetRangeandCountOutputAsString(currentRangeandCount,inputValues);
 			rangeFromReadingOutput.rangeValuesAndCount = currentRangeandCount;
 			rangeFromReadingOutput.stringIndexAndCount = rangeString;
 
 
-            // After finding the first range
-            // initialize the length by 1 to
-            // build the next range.
-            length = 1;
-			
-        }
-        else
-        {
-            length++;
-			
-        }
-		
+			// After finding the first range
+			// initialize the length by 1 to
+			// build the next range.
+			length = 1;
+
+		}
+		else
+		{
+			length++;
+
+		}
+
 		combined_range_count_list.push_back(rangeFromReadingOutput);
-    }
+	}
 
 
-    return combined_range_count_list;
+	return combined_range_count_list;
 }
+
 string GetRangeandCountOutputAsString(RangeValuesAndCount currentRangeandCount,int inputValues[] )
 {
 	int rangeLower = currentRangeandCount.rangeLower ;
-    int rangeUpper = currentRangeandCount.rangeUpper ;
+	int rangeUpper = currentRangeandCount.rangeUpper ;
 	int count            = currentRangeandCount.count;
-    string temp = "[" + to_string(rangeLower) + "-" + to_string(rangeUpper) + "]" + "->" + to_string(count) ;
+	string temp = "[" + to_string(rangeLower) + "-" + to_string(rangeUpper) + "]" + "->" + to_string(count) ;
 	if(rangeUpper == rangeLower)
 	{
 		temp = "[" + to_string(rangeLower) + "]"+ "->" + to_string(count);
@@ -84,6 +85,7 @@ string GetRangeandCountOutputAsString(RangeValuesAndCount currentRangeandCount,i
 	//range_list.push_back(temp);
 	return temp;
 }
+
 
 
 
