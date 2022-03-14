7997bfa1c50b458e631f2c2ec57ca0c1cad4c7c1
SreelakshmiSDilip <91017915+SreelakshmiSDilip@users.noreply.github.com>
2022-03-15 00:03:02 +0530

    Update TestRangeandFrequencyofCurrentReadings.cpp

--- TestRangeandFrequencyofCurrentReadings.cpp
+++ TestRangeandFrequencyofCurrentReadings.cpp
@@ -1,7 +1,7 @@
 
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 
-#include "test/catch.hpp"
+#include "test./catch.hpp"
 #include <assert.h>
 #include <iostream>
 #include "RangeandFrequencyofCurrentReadings.h"
