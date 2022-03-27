da4ba3218cf8c5c463a553d4c89de1797a47e16b
SreelakshmiSDilip <91017915+SreelakshmiSDilip@users.noreply.github.com>
2022-03-27 16:53:01 +0530

    Update RangeandFrequencyofCurrentReadings.cpp

--- RangeandFrequencyofCurrentReadings.cpp
+++ RangeandFrequencyofCurrentReadings.cpp
@@ -19,7 +19,7 @@ bool IsInputValid(int digitalInput)
 
 int convertDigitaltoAnalog(int digitalInput)
 {
-	float analogVal = (c_ANALOGMAXCURRENTOUTPUT * digitalInput)/c_DIGIMAXCURRENTINPUT ;
+	float analogVal = float( (c_ANALOGMAXCURRENTOUTPUT * digitalInput)/c_DIGIMAXCURRENTINPUT) ;
 	cout<<analogVal;
 	return(round(analogVal));
 
